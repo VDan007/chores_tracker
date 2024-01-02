12986c8eec43c6dc6d9234b6d21b8c3367cecaf6
DanielVarjaskéri <varjaskerid@gmail.com>
2024-01-02 22:49:11 +0100

    progress5

--- views/start.view.php
+++ views/start.view.php
@@ -1,7 +1,7 @@
 
 
 
-<div class="grow border flex justify-center items-center gap-5">
+<div class="grow  flex justify-center items-center gap-5">
     <a class="block" href="login.php">Login</a>
     <a class="block" href="">Sign up</a>
 
