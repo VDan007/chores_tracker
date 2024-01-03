a8577580f2427d48e05c0e5dc18d6208a2794b0e
DanielVarjaskéri <varjaskerid@gmail.com>
2024-01-03 23:02:45 +0100

    progress06

--- views/details.view.php
+++ views/details.view.php
@@ -1,5 +1,15 @@
 
+<?php
 
+    $status = $model->status;
+
+    $array_of_options = ['not started','in progress','delay','done'];
+    
+    $array_of_options_without_current_status = array_filter($array_of_options, fn($value) => $value != $status );
+
+    $array_to_display = array_merge([$model->status],$array_of_options_without_current_status);
+
+?>
 
 
 <form method="POST" class="flex flex-col items-center">
@@ -7,9 +17,9 @@
     <h3>Creation date: <?= $model->creation_date ?> </h3>
     <h3>Due date:<?= $model->due_date ?> </h3>
     <select name="status" id="">
-        <option value="">c</option>
-        <option value="">v</option>
-        <option value="">b</option>
+        <?php foreach($array_to_display as $item) : ?>
+            <option value="<?= $item ?>"><?= $item ?></option>
+        <?php endforeach; ?>
     </select>
     <p>Description: <?= $model->description ?>  </p>
 </form>
