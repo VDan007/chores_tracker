bf110f804be7a71e6b631d164096c07ae5133283
DanielVarjaskéri <varjaskerid@gmail.com>
2024-01-06 13:06:39 +0100

    nice

--- views/signup.view.php
+++ views/signup.view.php
@@ -13,18 +13,51 @@
     <input type="email" class="border" name="email" id="email" placeholder="Your email goes here">
 </label>
 
-<label for="password">Password
+<label for="password">Your password
     <input type="password" name="password" id="password" class="border">
 </label>
 
-<label for="password_again">Password again
+<label for="password_again">Your password again
     <input type="password" class="border" name="passord_again" id="password_again">
 </label>
 
-<label for="group">Group 
+
+
+<label for="join"> Join
+    <input type="radio" name="create_or_join" checked="true" value="join" id="join">
+</label>
+<label for="create"> Create
+    <input type="radio" name="create_or_join" value="create" id="create">
+</label>
+
+<label for="group">Group name
     <input type="text" name="group" id="group" class="border" placeholder="name">
+</label>
+
+<label for="group_password">Group password
+    <input class="border" type="password" id="group_password" name="group_password">
+</label>
+
+<label id="label_for_group_password_again" for="group_password_again">Group password again
+    <input class="border" type="password" id="group_password_again" name="group_password_again">
 </label>
 
 </form>
 
 </div>
+
+<script defer>
+    const join = document.getElementById('join');
+    const create = document.getElementById('create');
+    const labelForGroupPasswordAgain = document.getElementById('label_for_group_password_again');
+    join.addEventListener('change',toggleVisibility);
+    create.addEventListener('change',toggleVisibility);
+    toggleVisibility();
+    
+    
+
+    function toggleVisibility(){
+       // console.log('works');
+      join.checked  ? labelForGroupPasswordAgain.style.display = 'none' : labelForGroupPasswordAgain.style.display = 'inline';
+    }
+</script>
