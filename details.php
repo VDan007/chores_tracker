6e9b90fb111a342ad87f19b15c1241ddfd80a5a1
DanielVarjaskéri <varjaskerid@gmail.com>
2024-01-09 00:25:07 +0100

    comments are wworking

--- details.php
+++ details.php
@@ -12,15 +12,27 @@ if(is_get() && isset($_GET['id'])){
 
     $chore_i_need = find_chore_by_id($chores,$id);
 
-    view('details',$chore_i_need);
+    $comment_ids = json_decode($chore_i_need->comments);
+    if($comment_ids !== null){
+        $array_of_comment_objects = array_map(fn($id)=> Data::get_comment_by_id($id)[0],$comment_ids);
+        }else{
+    $array_of_comment_objects = [];
+    }
+
+    view('details',$chore_i_need,$array_of_comment_objects);
     
-}elseif(is_post()){
+}elseif(is_post() ){
    $status = sanitize($_POST['status']);
    $id = sanitize($_POST['id']);
 
    Data::update_chore($id,$status);
-   redirect('dashboard.php');
-
+   if(!isset($_POST['add_comment'])){
+       redirect('dashboard.php');
+   }else{
+    $comment = sanitize($_POST['new_comment']);
+    Data::add_comment($id,$comment,$_SESSION['user_logged_in']->email);
+    redirect("details.php?id=$id");
+   }
 
 }else{
     redirect('dashboard.php');
--- views/details.view.php
+++ views/details.view.php
@@ -27,5 +27,25 @@
     <input type="hidden" name="id" value="<?= $model->id ?>">
     <p>Description: <?= $model->description ?>  </p>
     <input class="cursor-pointer border bg-gray-300 p-1" type="submit" value="save changes">
+    <label class="flex" for="comment">Add a Comment:
+        <textarea class="border min-h-[150px] min-w-[280px]" name="new_comment" type="textarea"></textarea>
+    </label>
+    <button type="submit" name="add_comment" class="cursor-pointer bg-yellow-300 p-1"> add comment </button>
 </form>
 <a class="bg-red-300 block mx-auto my-5 max-w-[150px] text-center p-1" href="dashboard.php">back to dashboard</a>
+
+<div class="flex flex-col items-center mt-9 border">
+    <h2>Comments</h2>
+
+    <div>
+        <?php foreach($model2 as $comment) : ?>
+            <div class="border max-w-[330px] overflow-hidden">
+                <p>
+                    <?= $comment->comment ?>
+                </p>
+                <p>Author: <?= $comment->writer ?></p>
+            </div>
+        <?php endforeach ; ?>
+    </div>
+
+</div>
