59ea51628088e02c904b38d66f1f01ad1b6175af
DanielVarjaskéri <varjaskerid@gmail.com>
2024-01-11 01:02:03 +0100

    done like

--- views/setup.view.php
+++ views/setup.view.php
@@ -6,7 +6,7 @@
     <p class="text-2xl text-center"> TAKE A LOOK AT APP/CONFIG AND MODIFY AS NEEDED. </p> 
     <p class="text-2xl text-center"> TO SET UP DATABASE TABLES PRESS THE BUTTON. </p> 
     <form class="flex justify-center " action="" method="POST">
-        <input class="block min-h-[120px] min-w-[120px] border launch bg-contain rounded-md cursor-pointer"  type="submit" name="launch">
+        <input class="block min-h-[120px] min-w-[120px] border launch bg-contain rounded-md cursor-pointer" value="" type="submit" name="launch">
     </form>
     <?php
          if(isset($view_bag['db_setup_result'])){
